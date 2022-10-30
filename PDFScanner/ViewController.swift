6dbf09a943c2b0d77965c3c48cb15de89904e4e8
marcoalonsorota <marcoalonsorota@hotmail.com>
2022-10-30 12:57:05 -0600

    Empezando

--- PDFScanner/ViewController.swift
+++ PDFScanner/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import Vision
+import VisionKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
 
+    
+    @IBOutlet weak var previewDoc: UIImageView!
+    
+    //MARK: Variables
+    let scanVC = VNDocumentCameraViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scanVC.delegate = self
+        present(scanVC, animated: true)
     }
 
+    //MARK: Delegate
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        if scan.pageCount > 0 {
+            
+            previewDoc.image = scan.imageOfPage(at: 0)
+            
+         controller.dismiss(animated: true)
+        }
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        controller.dismiss(animated: true) {
+            let alerta = UIAlertController(title: "ATENCION", message: "", preferredStyle: .alert)
+            let accionAceptar = UIAlertAction(title: "OK", style: .default) { _ in
+                //Do something
+            }
+            alerta.addAction(accionAceptar)
+            self.present(alerta, animated: true)
+        }
+    }
 
 }
 
